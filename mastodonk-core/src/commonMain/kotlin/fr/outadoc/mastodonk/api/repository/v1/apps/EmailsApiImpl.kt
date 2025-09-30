9e772dc07be73e6172a6ba3e82f0016d438fbb06
Sanny S. <64450465+SpokeSugar@users.noreply.github.com>
2025-09-30 21:10:55 +0900

    fix: use setBody instead of submitForm to send JSON
    The `submitForm` extension was incorrectly attempting to send form-encoded data instead of JSON, resulting in a `400 Bad Request`.
    This commit fixes the issue by using `setBody` with `contentType(ContentType.Application.Json)` to correctly send JSON data.

--- mastodonk-core/src/commonMain/kotlin/fr/outadoc/mastodonk/api/repository/v1/apps/EmailsApiImpl.kt
+++ mastodonk-core/src/commonMain/kotlin/fr/outadoc/mastodonk/api/repository/v1/apps/EmailsApiImpl.kt
@@ -3,6 +3,7 @@ package fr.outadoc.mastodonk.api.repository.v1.apps
 import fr.outadoc.mastodonk.api.endpoint.v1.apps.EmailsApi
 import fr.outadoc.mastodonk.client.MastodonHttpClient
 import io.ktor.client.request.forms.*
+import io.ktor.client.request.setBody
 import io.ktor.http.*
 
 internal class EmailsApiImpl(private val client: MastodonHttpClient) : EmailsApi {
